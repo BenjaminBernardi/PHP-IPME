e164d82f7eed27b49bebf22e84216dbafadf6f30
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-06-18 09:49:09 +0200

    save

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$myVar = "Hello tout le monde !";
+
+// echo permet d'afficher des éléments sur la page.
+echo $myVar;
+
+// permet d'écrire un paragraphe contenant la valeur de $myVar.
+echo "<p>" . $myVar . "<p>";
+
+$tableau = [20, 12, 23];
+var_dump($tableau);
+
+// ne fonctionne pas !
+// echo $tableau;
+
+// boucle for
+// affiche chaque élément du tableau les uns à la suite des autres.
+for($i = 0; $i < count($tableau); $i++) {
+    echo $tableau[$i] . " ";
+}
+
+// boucle foreach
+foreach($tableau as $item) {
+    echo $item;
+}
+
+?>
