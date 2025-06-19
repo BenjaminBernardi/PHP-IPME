a127a9b2b99f5672ca01866c6b596b1dd0a3619b
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-06-19 11:08:26 +0200

    exo 1 et 2

---
+++ TP jour 2/exos_associatif/exo1.php
@@ -0,0 +1,13 @@
+<?php
+
+$personne = [
+    "nom" => "Bernardi",
+    "prenom" => "Benjamin",
+    "age" => 30
+];
+
+echo "<p>Nom : " . $personne["nom"] . "</p>";
+echo "<p>Prénom : " . $personne["prenom"] . "</p>";
+echo "<p>Âge : " . $personne["age"] . "</p>";
+
+?>
---
+++ TP jour 2/exos_associatif/exo2.php
@@ -0,0 +1,12 @@
+<?php
+
+$personne = [
+    "nom" => "Bernardi",
+    "prenom" => "Benjamin",
+    "age" => 26,
+    "ville" => "Paris"
+];
+
+var_dump($personne);
+
+?>
