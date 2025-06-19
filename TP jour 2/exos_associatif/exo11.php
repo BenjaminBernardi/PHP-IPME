aa2d4efd55390cf45319894b995bdb77fc8566e7
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-06-19 16:25:24 +0200

    update exo 11

--- TP jour 2/exos_associatif/exo11.php
+++ TP jour 2/exos_associatif/exo11.php
@@ -10,6 +10,18 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
             crossorigin="anonymous"></script>
+    <style>
+        img {
+            max-height: 500px;
+        }
+
+        .dead {
+            position: absolute;
+            top: 25%;
+            left: 25%;
+            max-width: 200px;
+        }
+    </style>
     <title>API Harry Potter</title>
     <?php
     $api = file_get_contents("https://hp-api.onrender.com/api/characters");
@@ -25,6 +37,9 @@
             <h5 class="card-title">Name: <?=$character["name"]?></h5>
             <h6>House: <?=$character["house"]?></h6>
             <p>Date of birth: <?=$character["dateOfBirth"]?></p>
+            <?php if ($character["alive"] == false) : ?>
+            <img class="dead" src="https://www.autocollant-tuning.com/1661-large_default/sticker-tete-de-mort.jpg" />
+            <?php endif; ?>
         </div>
         <?php endif; endforeach; ?>
     </div>
