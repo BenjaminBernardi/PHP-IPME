38e9d785209f48edac2b0118772b386d706aca5e
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-06-23 16:27:11 +0200

    save

--- TP jour 4/exos_associatif/form-treatment.php
+++ TP jour 4/exos_associatif/form-treatment.php
@@ -1,8 +1,23 @@
 <?php
 if (isset($_POST["country"]) && $_POST["country"] != "") {
-    $trimmed = trim($_POST["country"]);
-    $api = json_decode(file_get_contents("https://restcountries.com/v3.1/name/" . $trimmed . "?fullText=true"), true);
+    $trimmed = rawurlencode(trim($_POST["country"]));
+    $test = @file_get_contents("https://restcountries.com/v3.1/name/" . $trimmed . "?fullText=true");
+    if ($test == false) {
+        header("Location: http://localhost:8080/exos_associatif/form-country.php");
+        exit;
+    } else {
+        $test = file_get_contents("https://restcountries.com/v3.1/name/" . $trimmed . "?fullText=true");
+        $api = (json_decode(file_get_contents("https://restcountries.com/v3.1/name/" . $trimmed . "?fullText=true"), true));
+        foreach ($api as $country) : if ($country["name"]["official"] != "") : ?>
+            <h2><?= htmlspecialchars($country["name"]["official"]) ?></h2>
+            <h3>Capital : <?= htmlspecialchars($country["capital"][0]) ?></h3>
+            <p>Population : <?= htmlspecialchars(number_format($country["population"], 0, ",", " ")) ?></p>
+            <p>Region : <?= htmlspecialchars($country["region"]) ?></p>
+            <img src=<?= htmlspecialchars($country["flags"]["png"]) ?>>
+        <?php else : ?>
+            <h2>Pays introuvable</h2>
+        <?php endif; endforeach;
+        var_dump($api);
+    }
 }
-
-var_dump($api);
 ?>
