884095376fcf40122fc53918272d4c26e4043f81
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-06-26 12:01:37 +0200

    save

--- TP database/projet_api/index.php
+++ TP database/projet_api/index.php
@@ -16,6 +16,16 @@ if (isset($_GET["standard_state"]) && $_GET["standard_state"] == "gas") {
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} elseif (isset($_GET["search"]) && isset($_GET["block"]) && $_GET["block"] != "display+all") {
+    $sql = "SELECT * FROM elements WHERE name LIKE '%" . $_GET["search"] . "%' AND group_block = '" . $_GET["block"] . "' ORDER BY atomic_number";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} elseif (isset($_GET["search"]) && isset($_GET["block"]) && $_GET["block"] == "display+all") {
+    $sql = "SELECT * FROM elements WHERE name LIKE '%" . $_GET["search"] . "%' ORDER BY atomic_number";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } else {
     $sql = "SELECT * FROM elements ORDER BY atomic_number";
     $stmt = $pdo->prepare($sql);
@@ -23,30 +33,49 @@ if (isset($_GET["standard_state"]) && $_GET["standard_state"] == "gas") {
     $elements = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-// SELECT group_block, count(*) FROM elements GROUP BY group_block;
-//recuperer les éléments unique des standard :
-$test = [
-        'halogen', 'transition metal'
-]
+
+// recuperer les éléments de group_block :
+$sql = "SELECT group_block, count(*) FROM elements GROUP BY group_block";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$groupBlock = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$test = ["display all"];
+foreach ($groupBlock as $group) {
+    $test[] = $group["group_block"];
+}
 ?>
+
 <form id="recherche">
-    <select name="block">
-        <?php foreach ($test as $item) : ?>
-            <option value="<?php echo $item ?>"><?php echo $item ?></option>
-        <?php endforeach; ?>
-    </select>
-    <a href="index.php?standard_state=gas">
-        <button type="button">Show gas elements</button>
-    </a>
-    <a href="index.php?standard_state=solid">
-        <button type="button">Show solid elements</button>
-    </a>
-    <a href="index.php?standard_state=liquid">
-        <button type="button">Show liquid elements</button>
-    </a>
-    <a href="index.php">
-        <button type="button">Show all elements</button>
-    </a>
+    <div>
+        <label for="search">Search an element: </label>
+        <input type="search" name="search">
+    </div>
+    <div>
+        <label for="block">Sort by group block: </label>
+        <select name="block">
+            <?php foreach ($test as $item) : ?>
+                <option value="<?php echo $item ?>"><?php echo $item ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+    <div>
+        <input type="submit" value="Search an element">
+    </div>
+    <div>
+        <a href="index.php?standard_state=gas">
+            <button type="button">Show gas elements</button>
+        </a>
+        <a href="index.php?standard_state=solid">
+            <button type="button">Show solid elements</button>
+        </a>
+        <a href="index.php?standard_state=liquid">
+            <button type="button">Show liquid elements</button>
+        </a>
+        <a href="index.php">
+            <button type="button">Show all elements</button>
+        </a>
+    </div>
 </form>
 
 <table>
@@ -83,7 +112,3 @@ $test = [
         </tr>
     <?php endforeach; ?>
 </table>
-
-<a href="addelements.php" onclick="return confirm('Êtes-vous sûr de vouloir ajouter des éléments ?')">
-    <button type="button">Ajouter des éléments</button>
-</a>
